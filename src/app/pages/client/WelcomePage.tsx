5844209beecd0e4cc48ce0865c473729ecce197a
Krishan <33421343+kfiven@users.noreply.github.com>
2025-02-17 12:07:07 +1100

    Release v4.3.2 (#2213)

--- src/app/pages/client/WelcomePage.tsx
+++ src/app/pages/client/WelcomePage.tsx
@@ -24,7 +24,7 @@ export function WelcomePage() {
                   target="_blank"
                   rel="noreferrer noopener"
                 >
-                  v4.3.0
+                  v4.3.2
                 </a>
               </span>
             }
